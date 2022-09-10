5f9942877be63edc85e15522c9f378854afca995
Masterkartel <lennxkoros@gmail.com>
2022-09-10 15:28:29 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+/**
+ * main-prints the alphabets in lowercase
+ * followed by a newline
+ * Return:Always 0 (success)
+ */
+
+int main(void)
+{
+	char ch;
+
+	for(ch='a';ch<='z'; ch++)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
