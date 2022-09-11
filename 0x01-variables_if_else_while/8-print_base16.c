ca7ee6e47d39aacb72fbc514957743b4b4045a11
Masterkartel <lennxkoros@gmail.com>
2022-09-11 11:21:13 -0700

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+
+/**
+ * main-prints all the numbers of bas 16 in lowercase,
+ * followed by a new line
+ * Putchar can only be used thrice
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int n;
+	char ch;
+
+	for (n = 48; n <= 58; n++)
+	{
+		putchar(n);
+	}
+	for (ch = 'a'; ch <= 'f'; ch++)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
