405f4c350c60f3536f7ab4d5bf6fd8c7cf297ee2
Masterkartel <lennxkoros@gmail.com>
2022-09-21 14:50:27 -0700

    2-strlen.c

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -4,7 +4,7 @@
  * _strlen - returns the lenght of a string
  * @s: string to evaluate
  *
- * Return: the lenght of the string
+ * Return: the length of the string
  */
 int _strlen(char *s)
 {
@@ -16,6 +16,6 @@ int _strlen(char *s)
 	{
 		i++;
 	}
-	
+
 	return (i);
 }
